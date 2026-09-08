9716cd7e224f9acc778cff2391259f6f9ce5c992
Iman-gholami <69814075+Iman-gholami@users.noreply.github.com>
2026-09-08 09:51:27 +0330

    Avoid stale intelligence dataset state after atomic imports

--- src/repositories/IpAssetRepository.js
+++ src/repositories/IpAssetRepository.js
@@ -5,7 +5,7 @@ class IpAssetRepository {
   constructor({
     assetModel = IpAsset,
     stateModel = IntelDatasetState,
-    stateTtlMs = 30000,
+    stateTtlMs = 0,
   } = {}) {
     this.assetModel = assetModel;
     this.stateModel = stateModel;
@@ -15,7 +15,11 @@ class IpAssetRepository {
 
   async getActiveState() {
     const now = Date.now();
-    if (this.stateCache && now - this.stateCache.loadedAt < this.stateTtlMs) {
+    if (
+      this.stateTtlMs > 0 &&
+      this.stateCache &&
+      now - this.stateCache.loadedAt < this.stateTtlMs
+    ) {
       return this.stateCache.value;
     }
 

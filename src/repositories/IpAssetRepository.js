e3a7842f626dc83445990e48e47599d2d7b5cca7
Iman-gholami <69814075+Iman-gholami@users.noreply.github.com>
2026-09-08 09:41:05 +0330

    Add local intelligence repositories

---
+++ src/repositories/IpAssetRepository.js
@@ -0,0 +1,46 @@
+const IpAsset = require("../models/IpAsset");
+const IntelDatasetState = require("../models/IntelDatasetState");
+
+class IpAssetRepository {
+  constructor({
+    assetModel = IpAsset,
+    stateModel = IntelDatasetState,
+    stateTtlMs = 30000,
+  } = {}) {
+    this.assetModel = assetModel;
+    this.stateModel = stateModel;
+    this.stateTtlMs = stateTtlMs;
+    this.stateCache = null;
+  }
+
+  async getActiveState() {
+    const now = Date.now();
+    if (this.stateCache && now - this.stateCache.loadedAt < this.stateTtlMs) {
+      return this.stateCache.value;
+    }
+
+    const value = await this.stateModel
+      .findOne({ dataset: "asset_registry" })
+      .lean()
+      .exec();
+
+    this.stateCache = { value, loadedAt: now };
+    return value;
+  }
+
+  invalidateStateCache() {
+    this.stateCache = null;
+  }
+
+  async findByIp(ip) {
+    const state = await this.getActiveState();
+    if (!state?.activeImportId) return null;
+
+    return this.assetModel
+      .findOne({ importId: state.activeImportId, ip })
+      .lean()
+      .exec();
+  }
+}
+
+module.exports = { IpAssetRepository };
